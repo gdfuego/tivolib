7949d8ba3311f35917371ed184748c289b46196e
G.D. Fuego <gdfuego@gmail.com>
2014-02-14 21:55:17 -0500

    Initial commit of the current version

---
+++ tivo.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+
+###
+# Simple example for using the tivolib library
+##
+
+import os
+import sys
+import tivolib
+import re
+
+def setup():
+    from ConfigParser import ConfigParser
+    from optparse import OptionParser
+    options = {}
+    options['tivo'] = ''
+    options['media'] = 0
+    options['decrypt'] = False
+    options['encode'] = False
+    # Read the config file
+    config = ConfigParser()
+    config.read([os.path.expanduser('~/.tivorc')])
+    try:
+        options['tivo'] = config.get('Tivo', 'host')
+    except:
+        pass
+    try:
+        options['media'] = config.get('Tivo', 'MAC')
+    except:
+        pass
+    try:
+        options['decrypt'] = config.getboolean('Tivo', 'decrypt')
+    except:
+        pass
+    try:
+        options['encode'] = config.getboolean('Tivo', 'encode')
+    except:
+        pass
+    try:
+        options['storage'] = config.get('Tivo', 'storage')
+    except:
+        pass
+
+    # Read commandline options
+    parser = OptionParser('usage: %prog [options] tivo')
+    parser.add_option('-d', action='store_true', dest='decrypt',
+        help='Decrypt the file automatically')
+    parser.add_option('-e', action='store_true', dest='encode',
+        help='Re-Encode the video.  Implies decrypt. (currently broken)')
+    parser.add_option('-m', '--media',
+        help='Media Access Code from TiVo (required).')
+    parser.add_option('-s', '--storage',
+        help="Location to store downloaded files")
+    (optionlist, args) = parser.parse_args()
+    if (len(args) == 1):
+        options['tivo'] = args[0]
+    if options['tivo'] == '':
+        parser.error('You must specify a Tivo device to connect to')
+    if (not optionlist.media) and (options['media'] == 0):
+        parser.error('You must specify a Media Access Code (-m)')
+    if optionlist.media:
+        options['media'] = optionlist.media
+    if optionlist.decrypt:
+        options['decrypt'] = optionlist.decrypt
+    if optionlist.encode:
+        options['encode'] = optionlist.encode
+    if optionlist.storage:
+        options['storage'] = optionlist.storage
+    return options
+
+
+def progress(current, total):
+    """Simple progress indicator"""
+    import sys
+    percentcomplete = current * 100 / total
+    sys.stdout.write("\r  " + "%d%% complete" % percentcomplete)
+    sys.stdout.flush()
+
+def printshow(show):
+    size = str(int(show['SourceSize']) / 1024 / 1024) + " MB"
+    showinfo = size.rjust(10) + "   "
+    if show.has_key('InProgress'):
+      showinfo += "[In-Progress] "
+    if show.has_key('CopyProtected'):
+      showinfo += "[CopyProtected] "
+    if show.has_key('HighDefinition'):
+      if show['HighDefinition'] == "Yes":
+        showinfo += "[HD] "
+    showinfo += showName(show)
+    return showinfo
+
+def showName(show):
+    name = show['Title']
+    if show.has_key('EpisodeTitle'):
+        name += ':'
+        if show.has_key('EpisodeNumber') and show['EpisodeNumber'] != "0":
+            name += ' [Ep. ' + show['EpisodeNumber'] + "]"
+        name += ' ' + show['EpisodeTitle']
+    return name
+
+def main():
+    options = setup()
+    if options.has_key('storage'):
+        path = options['storage']
+    else:
+        path = '.'
+    myTivo = tivolib.tivoHandler(options['tivo'], options['media'])
+    shownum = 1
+    shows = myTivo.listshows()
+    for show in shows:
+        print str(shownum).ljust(5), 
+        print printshow(show).encode('utf-8')
+        shownum = shownum + 1
+
+    while 1:
+        print
+        download = input('Choose show to download (0 to quit): ')
+        try:
+            download = int(download)
+            break
+        except:
+            print
+            print('You must enter a number.')
+
+    if download > 0:
+        myShow = shows[download-1]
+        name = showName(myShow)
+        # Remove unsafe filename characters
+        name = re.sub('[*!]', '', name)
+        if options['decrypt']:
+            filename = name + '.mpeg'
+            if options['encode']:
+                filename = name + '.ogv'
+        else:
+            filename = name + '.TiVo'
+        print
+        print ('Downloading ' + name)
+        print
+        if myTivo.download(myShow, filename, path=path, decrypt=options['decrypt'], encode=options['encode'], prog=progress):
+            print('\nDownload Complete')
+        else:
+            print('Download Failed')
+
+
+if __name__ == '__main__':
+    main()
---
+++ tivolib.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+
+__version__ = "0.8"
+__author__ = "Gregory Boyce <gboyce@badbelly.com>"
+__copyright = "(c) Coypright 2010 by Gregory Boyce <gboyce@badbelly.com>"
+__license__ = "GPL"
+
+
+class tivoHandler():
+    """Class for handling TiVo connections"""
+
+    def __init__(self, mytivo, media):
+        """Setup connection to the TiVo.
+        Accepts mytivo variable containing the IP address or hostname of a Tivo
+        device and media variable containing your Media Access Key"""
+        self.username = "tivo"
+        self.tivo = mytivo
+        self.media = media
+        self.connect()
+
+    def connect(self):
+        """Establish the initial connection"""
+	import urllib2
+	import cookielib
+        self.cj = cookielib.CookieJar()
+        self.ck = cookielib.Cookie(version=0, name='sid', value='0000000000000000', port=None, port_specified=False, domain=self.tivo, domain_specified=False, domain_initial_dot=False, path='/', path_specified=True, secure=False, expires=None, discard=True, comment=None, comment_url=None, rest={'HttpOnly': None}, rfc2109=False)
+        self.cj.set_cookie(self.ck)
+        self.urlhandler = urllib2
+        self.authhandler = self.urlhandler.HTTPDigestAuthHandler()
+        self.authhandler.add_password("TiVo DVR", self.tivo, \
+            self.username, self.media)
+        self.opener = self.urlhandler.build_opener(self.authhandler, \
+            self.urlhandler.HTTPCookieProcessor(self.cj))
+        self.urlhandler.install_opener(self.opener)
+        try:
+            self.pagehandle = self.urlhandler.urlopen("https://" + self.tivo + \
+              "/nowplaying/index.html")
+        except IOError as e:
+            if hasattr(e, 'code'):
+                if e.code != 401:
+                    print('We got another error')
+                    print(e.code)
+            else:
+                print("Error 401")
+                print(e.headers)
+                print(e.headers['www-authenticate'])
+
+    def listshows(self):
+        """Obtain a list of shows"""
+        self.pagehandle = self.urlhandler.urlopen("https://" + self.tivo + \
+            "/TiVoConnect?Container=%2FNowPlaying&Command=QueryContainer&Recurse=Yes")
+        self.list = self.pagehandle.readlines()[0]
+        self.shows = showParser(self.list)
+        self.shows.sort(key=lambda show: show['Title'])
+        return self.shows
+
+    def progress(current, total):
+        """Stub Progress indicator"""
+        return
+
+    def download(self, show, filename, path=".", decrypt=False, encode=False, prog=progress):
+        """Trigger a download for the specified TV show.
+        Takes a show object as an argument."""
+        import sys
+        self.myshow = show
+        self.fullpath = path + "/" + filename
+        self.fd = open(self.fullpath, 'wb')
+        if encode:
+            self.fd = tivoencode(self.fd)
+            if self.fd == False:
+              return False
+        if decrypt or encode:
+            self.fd = tivodecrypt(self.fd, self.media)
+            if self.fd == False:
+              return False
+        self.pagehandle = self.urlhandler.urlopen(self.myshow['Url'])
+        headers = self.pagehandle.info().headers
+        for header in headers:
+	          if header.split(" ")[0] == "TiVo-Estimated-Length:":
+	              self.filesize = int(header.split(" ")[1])
+        self.bs = 512 * 1024 # .5MB at a time
+        self.numblocks = self.filesize // self.bs
+        for count in range(1, self.numblocks):
+            prog(count, self.numblocks)
+            self.block = self.pagehandle.read(self.bs)
+            self.fd.write(self.block)
+        prog(self.numblocks, self.numblocks)
+        self.block = self.pagehandle.read(self.filesize % self.bs)
+        self.fd.write(self.block)
+        self.fd.close()
+        return True
+
+
+def showParser(data):
+    """Take a Tivo Manifest and return a list show arrays"""
+    import xml.dom.minidom
+    from re import sub
+    shows = []
+    dom = xml.dom.minidom.parseString(data)
+    for show in dom.getElementsByTagName('Item'):
+        myshow = {}
+        details = show.getElementsByTagName('Details')[0]
+        for node in details.childNodes:
+            if node.localName:
+                myshow[node.localName] = node.childNodes[0].data
+        Url = show.getElementsByTagName('Links')[0].getElementsByTagName('Url')
+        myshow['Url'] =  sub(":80", "", Url[0].childNodes[0].data)
+        shows.append(myshow)
+    return shows
+
+
+def tivodecrypt(outfile, media):
+    """Decode a TiVo file and output an MPEG file.
+    Takes the input file, output file and media access code (MAC) as arguments."""
+    import subprocess
+    try:
+      tivodecode = subprocess.Popen(['tivodecode', '-m', media, "-"],
+          stdin=subprocess.PIPE, stderr=open("/dev/null", 'w'),
+          stdout=outfile)
+    except OSError:
+      print "tivodecode not found."
+      return -1 
+    return tivodecode.stdin
+
+
+def tivoencode(outfile):
+   """Uses fmpeg to re-encode the movie optimized for an iPad.
+   Uses a recommended two step encoding process"""
+   import subprocess
+   try:
+     encoder = subprocess.Popen(['ffmpeg2theora', '-o', '-', '-'],
+       stdin=subprocess.PIPE, stderr=open("/dev/null", 'w'),
+       stdout=outfile)
+   except OSError:
+     print "ffmpeg2theora not found"
+     return False
+   return encoder.stdin
